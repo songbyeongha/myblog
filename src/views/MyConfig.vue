faa899f45deca3358371ff45388b020f332054b2
이대경 <coolbino@naver.com>
2019-07-29 11:46:19 +0900

    admin component 분리

--- src/views/MyConfig.vue
+++ src/views/MyConfig.vue
@@ -8,11 +8,11 @@
       <v-tabs vertical>
         <v-tab>
           <v-icon left>mdi-account</v-icon>
-          사용자목록
+          계정권한설정
         </v-tab>
         <v-tab>
           <v-icon left>mdi-lock</v-icon>
-          전체게시글
+          카운터
         </v-tab>
         <v-tab>
           <v-icon left>mdi-access-point</v-icon>
@@ -23,10 +23,7 @@
           <v-card flat>
             <v-card-text>
               <!-- Table -->
-      <v-flex xs12 mg12 lg12 text-xs-center>
-        <h2>사용자 권한 설정</h2>
-      </v-flex>
-      <UserTable></UserTable>
+              <UserTable></UserTable>
               
             </v-card-text>
           </v-card>
@@ -34,42 +31,8 @@
         <v-tab-item>
           <v-card flat>
             <v-card-text>
-                <!-- Total Count -->
-      <v-layout row wrap style="margin-top: 20px;">
-        <v-flex xs6 mg6 lg6 text-xs-center>
-          <h2>총 게시글 수</h2>
-        </v-flex>
-        <v-flex xs6 mg6 lg6 text-xs-center>
-          <v-progress-circular
-            indeterminate
-            color="primary"
-            v-if="loading"
-          ></v-progress-circular>
-          <h2 v-else>{{ sum }}</h2>
-        </v-flex>
-        <v-flex xs6 mg6 lg6 text-xs-center>
-          <h2>POSTS</h2>
-        </v-flex>
-        <v-flex xs6 mg6 lg6 text-xs-center>
-          <v-progress-circular
-            indeterminate
-            color="primary"
-            v-if="loading"
-          ></v-progress-circular>
-          <h2 v-else>{{ posts }}</h2>
-        </v-flex>
-        <v-flex xs6 mg6 lg6 text-xs-center>
-          <h2>PORTFOLIOS</h2>
-        </v-flex>
-        <v-flex xs6 mg6 lg6 text-xs-center>
-          <v-progress-circular
-            indeterminate
-            color="primary"
-            v-if="loading"
-          ></v-progress-circular>
-          <h2 v-else>{{ portfolios }}</h2>
-        </v-flex>
-      </v-layout>
+              <PostCounter></PostCounter>
+      
             </v-card-text>
           </v-card>
         </v-tab-item>
@@ -90,36 +53,17 @@
 import ImgBanner from "../components/ImgBanner";
 import UserTable from "../components/UserTable";
 import fbservice from "@/services/FirebaseService.js";
+import PostCounter from '../components/PostCounter.vue';
 import { clearInterval } from "timers";
 
 export default {
   name: "ConfigPage",
   components: {
     ImgBanner,
-    UserTable
+    UserTable,
+    PostCounter
   },
-  data() {
-    return {
-      posts: 0,
-      portfolios: 0,
-      loading: true
-    };
-  },
-  computed: {
-    sum() {
-      return this.posts + this.portfolios;
-    }
-  },
-  mounted() {
-    this.initialize();
-  },
-  methods: {
-    async initialize() {
-      this.posts = await fbservice.countPost();
-      this.portfolios = await fbservice.countPortfolio();
-      this.loading = false;
-    }
-  }
+ 
 };
 </script>
 
