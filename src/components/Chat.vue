3b1a43c839601175baf3a03e051eeb9042da4379
송병하 <qudgk0006@naver.com>
2019-07-29 16:24:36 +0900

    S1P3113006-15 단비 챗봇 기능 추가

---
+++ src/components/Chat.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="frogue-container" class="position-right-bottom"
+      data-chatbot="bdf98444-2784-48aa-90e9-c3c1ed65cddd"
+      data-user="azsw7797@gmail.com"
+      data-init-key="value"
+      ></div>
+</template>
+
+<script>
+export default {
+    created(){
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https:\/\/danbee.ai/js/plugins/frogue-embed/frogue-embed.min.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'frogue-embed'));
+    }
+}
+</script>
+
+<style>
+
+</style>
