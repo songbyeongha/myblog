a07406968299a7f26bc485797ed6606ce7b52578
신민석 <alstjr7388@naver.com>
2019-07-30 11:42:02 +0900

    v-card 추가 S1P3113006-25

--- src/views/PortfolioViewPage.vue
+++ src/views/PortfolioViewPage.vue
@@ -1,5 +1,30 @@
 <template>
-  <div v-html="compiledMarkdown"></div>
+  <v-layout class="portfolio" justify-center>
+    <v-flex xs11>
+      <v-card>
+        <v-img class="white--text" aspect-ratio="1.7" :src="getPortfolio.img">
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-content-center flexbox>
+                <span class="headline">{{ getPortfolio.title }}</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-img>
+        <v-card-title>
+          <div>
+            <span class="grey--text">{{ getPortfolio.name }}</span>
+            <br />
+            <span v-html="compiledMarkdown">Whitehaven Beach</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -18,10 +43,16 @@ export default {
       return store.state.portfolio;
     },
     compiledMarkdown() {
+      console.log(this.getPortfolio);
       return marked(this.getPortfolio.body);
     }
   }
 };
 </script>
 
-<style></style>
+<style scoped>
+.portfolio {
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+</style>
