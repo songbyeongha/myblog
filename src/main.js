a7b5af3c0c611df67cd171c033442c3ceac4a495
신민석 <alstjr7388@naver.com>
2019-08-07 17:31:34 +0900

    이미지 컴포넌트 수정 url만 받을수 있도록 수정 S1P3113006-48

--- src/main.js
+++ src/main.js
@@ -12,17 +12,20 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import vueResource from "vue-resource";
+import VueClipboard from "vue-clipboard2";
+
+Vue.use(VueClipboard);
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify, {
-	iconfont: "fa",
-	theme: {
-		primary: "#3f51b5",
-		secondary: "#b0bec5",
-		accent: "#8c9eff",
-		error: "#b71c1c"
-	}
+  iconfont: "fa",
+  theme: {
+    primary: "#3f51b5",
+    secondary: "#b0bec5",
+    accent: "#8c9eff",
+    error: "#b71c1c"
+  }
 });
 
 Vue.use(VueSimplemde);
@@ -30,7 +33,7 @@ Vue.use(vueResource);
 Vue.use(VueMomentJS, moment);
 
 new Vue({
-	router,
-	store,
-	render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app");
